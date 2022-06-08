b4bbc3c1cd52adb7b9a211df1c35f24fb700be42
hsp9308 <hsp9308@gmail.com>
2022-06-08 09:43:40 +0900

    Updated layers.py

--- common/layers.py
+++ common/layers.py
@@ -63,7 +63,59 @@ class Affine:
         self.grads[0][...] = dW
         self.grads[1][...] = db
         return dx
+
+
+# Word2vec의 연산을 간략화하기 위한 Embedding 계층
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout) # 중복문제 해결을 위해 해당 행에 할당이 아닌 더하기. dout를 dW의 idx번째 행에 더해줌
+        # 아래와 동일한 연산을 수행함.
+        # for i, word_id in enumerate(self.idx):
+        #   dW[word_id] += dout[i]
+        # 일반적으로 파이썬에서 for문 보다는 넘파이의 내장 메서드를 사용하는 편이 더 빠름. 
         
+        return None
+
+# 은닉 - 출력 연산 Layer.
+# Embedding과는 별개로 짬 (구조가 반대라서)
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx): # 은닉층 뉴런(h)과 단어 ID의 넘파이 배열(idx) (배열인 이유는 미니배치 처리 가정)
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1) # 내적
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout): # 순전파의 반대순서로 기울기를 전달해 구현
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
@@ -89,3 +141,29 @@ class SoftmaxWithLoss:
         dx = dx / batch_size
 
         return dx
+
+# Sigmoid 함수를 Loss function으로.
+# Softmax 함수를 조금 바꾸는 것으로 쉽게 구현 가능.
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None # loss function storaged.
+        self.y = None  # sigmoid output
+        self.t = None  # Answer label
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+
+        return dx
+
+
